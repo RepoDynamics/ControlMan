1a43f7a31d455dbaf802b8b58d3fe5ca3eb43e06
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-08-20 06:30:08 +0200

    fix

--- src/repodynamics/meta/files/package/__init__.py
+++ src/repodynamics/meta/files/package/__init__.py
@@ -5,7 +5,7 @@ def sync(sync_manager):
     language = sync_manager.metadata["package"]["language"]
     match language:
         case "python":
-            python.Python(sync_manager=sync_manager).update()
+            python.PackageFileSync(sync_manager=sync_manager).update()
         case _:
             raise NotImplementedError(f"Language '{language}' not implemented.")
     return
