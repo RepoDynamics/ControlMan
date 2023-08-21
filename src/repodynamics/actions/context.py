3b4d3afe968e5d7973da8b651edf543587240fad
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-08-21 02:12:09 +0200

    fix: context

--- src/repodynamics/actions/context.py
+++ src/repodynamics/actions/context.py
@@ -3,19 +3,48 @@ import json
 from markitup import html, md
 
 
-def context(github: dict) -> tuple[None, str]:
-    _ = github.pop("token")
+def context(
+    github: dict,
+    env: dict,
+    vars: dict,
+    job: dict,
+    jobs: dict,
+    steps: dict,
+    runner: dict,
+    strategy: dict,
+    matrix: dict,
+    needs: dict,
+    inputs: dict,
+) -> tuple[None, None, str]:
+    github["token"] = "***REDACTED***"
     payload_data = github.pop("event")
-    context_details = html.details(
-        content=md.code_block(json.dumps(dict(sorted(github.items())), indent=4), "json"),
-        summary="🖥 GitHub Context",
-        content_indent=""
+    summary = html.ElementCollection(
+        [
+            html.h(2, "Workflow Context"),
+            html.details(
+                content=md.code_block(json.dumps(dict(sorted(payload_data.items())), indent=4), "json"),
+                summary="📥 Event payload",
+            ),
+        ]
     )
-    payload_details = html.details(
-        content=md.code_block(json.dumps(dict(sorted(payload_data.items())), indent=4), "json"),
-        summary="🖥 Event Payload",
-        content_indent=""
-    )
-    return None, f"<h2>Workflow Context</h2>{context_details}\n{payload_details}"
-
-
+    for name, data in (
+        ("github", github),
+        ("env", env),
+        ("vars", vars),
+        ("job", job),
+        ("jobs", jobs),
+        ("steps", steps),
+        ("runner", runner),
+        ("strategy", strategy),
+        ("matrix", matrix),
+        ("needs", needs),
+        ("inputs", inputs),
+    ):
+        if data:
+            summary.append(
+                html.details(
+                    content=md.code_block(json.dumps(dict(sorted(data.items())), indent=4), "json"),
+                    summary=f"🖥 <code>{name}</code> context",
+                )
+            )
+    return None, None, str(summary)
