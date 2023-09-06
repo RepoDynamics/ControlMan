49f85065007c759d59ac96174e401e29edf885ad
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-09-06 23:46:56 +0200

    fix

--- src/repodynamics/actions/_print.py
+++ src/repodynamics/actions/_print.py
@@ -10,7 +10,10 @@ def main():
     parser.add_argument("details", nargs="?", type=str, help="Color to apply to the text.")
     args = parser.parse_args()
     logger = Logger("github")
-    logger.log(args.type, args.message, args.details)
+    if args.type in [f"h{i}" for i in range(1, 5)]:
+        logger._h(int(args.type[1]), args.message)
+    else:
+        logger.log(args.type, args.message, args.details)
 
 
 if __name__ == "__main__":
