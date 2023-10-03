8d5d7d56c4a497f42d848e810aa99bf75a6b5887
AAriam <80158628+AAriam@users.noreply.github.com>
2023-10-03 18:46:03 +0200

    Update cd.py

--- src/repodynamics/cd.py
+++ src/repodynamics/cd.py
@@ -29,18 +29,6 @@ class MergeLogger:
                 self.maintenance()
         return
 
-    def initial_release(self):
-
-
-
-        with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
-            print(f"release=true", file=fh)
-            print(f"docs=true", file=fh)
-            print(f"tag=v0.0.0", file=fh)
-        with open("RELEASE.md", "w") as f:
-            f.write(log)
-        return
-
     def release(self, release: list[bool], latest_tag: list[int]):
         log = f"## {self.event['pull_request']['title']}\n{self.event['pull_request']['body']}"
         with open("CHANGELOG.md", "a") as f:
@@ -75,10 +63,6 @@ class MergeLogger:
             print(f"release=false", file=fh)
         return
 
-    @property
-    def labels(self):
-        return [label["name"] for label in self.event["pull_request"]["labels"]]
-
     def update_type(self):
         release = [False, False, False]
         docs = False
@@ -90,30 +74,3 @@ class MergeLogger:
                     if label == f"Release: {release_type}":
                         release[segment] = True
         return release, docs
-
-
-def __main__():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=str, help="Path to the root directory.")
-    args = parser.parse_args()
-    MergeLogger(path_event_payload_file=args.path).run()
-    # with open("event_workload.json", "w") as f:
-    #     json.dump(event, f, indent=4)
-    # try:
-    #     meta = Metadata(
-    #         path_root=args.root,
-    #         path_pathfile=args.pathfile,
-    #         path_cache=args.cachefile,
-    #         update_cache=args.update_cache,
-    #     )
-    # except Exception as e:
-    #     print(f"Error: {e}")
-    #     sys.exit(1)
-    # # print(meta.json())
-    # meta.json(write_to_file=True, output_filepath=args.output)
-    # meta.json(write_to_file=True, output_filepath=args.output_pretty, indent=4)
-    return
-
-
-if __name__ == "__main__":
-    __main__()
