b0583a56dbc4c3368415340ffcbb3dcb1f153753
AAriam <80158628+AAriam@users.noreply.github.com>
2023-10-09 03:20:48 +0200

    Update reader.py

--- src/repodynamics/meta/reader.py
+++ src/repodynamics/meta/reader.py
@@ -19,41 +19,25 @@ class MetaReader:
     def __init__(
         self,
         path_root: str | Path = ".",
-        path_meta: str | Path = ".meta",
         github_token: Optional[str] = None,
         logger: Logger = None
     ):
         self.logger = logger or Logger()
         self.logger.h2("Process Meta Source Files")
-        self._path_root = Path(path_root).resolve()
         self._github_token = github_token
-        self._path_meta = self._path_root / path_meta
-        self._path_local = self._path_root / ".local"
-        self._path_meta_local = self._path_local / "meta"
-        self._path_api_cache = self._path_meta_local / "api_cache.yaml"
-        self._path_extensions_dir = self._path_meta_local / "extensions"
-        if not self._path_meta.is_dir():
-            self.logger.error(f"Input meta directory '{self._path_meta}' not found.")
-        if not self._path_extensions_dir.is_dir():
-            self.logger.error(
-                f"Could not find Local meta extensions directory at: '{self._path_extensions_dir}'."
-            )
-        self._metadata: dict = self._read_core_metadata()
-        self._extensions: list[dict] = self._metadata.get("extensions", [])
+        self._path = PathReader(path_root=path_root, logger=self.logger)
         self._local_config = self._get_local_config()
-        if self._extensions:
-            self._path_extensions, exists = self._get_local_extensions()
-            if not exists:
-                self._download_extensions(self._extensions, self._path_extensions)
-            self._update_metadata_from_extensions()
-        self._add_metadata_default_values()
-        self._validate_metadata()
+
+        self._extensions, self._path_extensions = self._read_extensions()
+        self._metadata: dict = self._read_raw_metadata()
+
         if self._metadata.get("package"):
             self._package_config = self._read_package_config()
         else:
             self._package_config = None
+
         self._cache: dict = self._initialize_api_cache()
-        self._db = self._read_yaml(_util.file.datafile("db.yaml"))
+        self._db = self._read_datafile(_util.file.datafile("db.yaml"))
         return
 
     def template(
@@ -79,7 +63,7 @@ class MetaReader:
                 f"{f'extension repository {extension_nr}' if extension_nr else 'main repository'}."
             )
             if category in ["issue", "discussion"]:
-                return self._read_yaml(path)
+                return self._read_datafile(path)
             with open(path) as f:
                 content = f.read()
             self.logger.success("File successfully loaded.", str(content))
@@ -146,154 +130,151 @@ class MetaReader:
         return
 
     def cache_save(self):
-        with open(self._path_api_cache, "w") as f:
+        with open(self.path.local_api_cache, "w") as f:
             YAML(typ="safe").dump(self._cache, f)
-        self.logger.success(f"Cache file saved at {self._path_api_cache}.")
+        self.logger.success(f"Cache file saved at {self.path.local_api_cache}.")
         return
 
     @property
-    def path_root(self) -> Path:
-        return self._path_root
+    def path(self):
+        return self._path
 
-    @property
-    def path_meta(self) -> Path:
-        return self._path_meta
+    def _read_extensions(self) -> tuple[dict, Path | None]:
+        extensions = _util.dict.read(
+            path=self.path.meta_core_extensions,
+            schema=self._get_schema("core/extensions"),
+            logger=self.logger
+        )
+        if not extensions["extensions"]:
+            return extensions, None
+        local_path, exists = self._get_local_extensions(extensions["extensions"])
+        if not exists:
+            self._download_extensions(extensions["extensions"], download_path=local_path)
+        return extensions, local_path
 
-    @property
-    def path_meta_local(self) -> Path:
-        return self._path_meta_local
-
-    def _get_local_extensions(self) -> tuple[Path, bool]:
+    def _get_local_extensions(self, extensions: list[dict]) -> tuple[Path, bool]:
         self.logger.h3("Get Local Extensions")
-        extention_defs = json.dumps(self._metadata["extensions"]).encode('utf-8')
+        extention_defs = json.dumps(extensions).encode('utf-8')
         hash = hashlib.md5(extention_defs).hexdigest()
         self.logger.info(f"Looking for non-expired local extensions with hash '{hash}'.")
         dir_pattern = re.compile(
             r"^(20\d{2}_(?:0[1-9]|1[0-2])_(?:0[1-9]|[12]\d|3[01])_(?:[01]\d|2[0-3])_[0-5]\d_[0-5]\d)__"
             r"([a-fA-F0-9]{32})$"
         )
-        for path in self._path_extensions_dir.iterdir():
+        for path in self.path.dir_local_meta_extensions.iterdir():
             if path.is_dir():
                 match = dir_pattern.match(path.name)
                 if match and match.group(2) == hash and not self._is_expired(match.group(1), typ="extensions"):
                     self.logger.success(f"Found non-expired local extensions at '{path}'.")
                     return path, True
         self.logger.info(f"No non-expired local extensions found.")
-        new_path = self._path_extensions_dir / f"{self._now}__{hash}"
+        new_path = self.path.dir_local_meta_extensions / f"{self._now}__{hash}"
         return new_path, False
+
+    def _download_extensions(self, extensions: list[dict], download_path: Path) -> None:
+        dest_path = {
+            "meta": ".",
+            "config": "config",
+            "pyproject": "config/pyproject",
+            "data": "data",
+            "template": "template",
+            "health_file": "template/health_file",
+            "license": "template/license",
+            "issue": "template/issue",
+            "discussion": "template/discussion",
+            "pull": "template/pull",
+            "media": "media",
+            "logo": "media/logo",
+        }
+        self.logger.h3("Download Meta Extensions")
+        _util.file.delete_dir_content(self.path.dir_local_meta_extensions, exclude=["README.md"])
+        for idx, extension in enumerate(extensions):
+            self.logger.h4(f"Download Extension {idx + 1}")
+            self.logger.info(f"Input: {extension}")
+            repo_owner, repo_name = extension['repo'].split("/")
+            dir_path = download_path / f"{idx + 1 :03}"
+            try:
+                extension_filepaths = self.github.user(repo_owner).repo(repo_name).download_content(
+                    path=extension.get('path', ''),
+                    ref=extension.get('ref'),
+                    download_path=dir_path / dest_path[extension['type']],
+                    recursive=True,
+                    keep_full_path=False,
+                )
+            except WebAPIPersistentStatusCodeError as e:
+                self.logger.error(f"Error downloading extension data:", str(e))
+            if not extension_filepaths:
+                self.logger.error(f"No files found in extension.")
+            else:
+                self.logger.success(
+                    f"Downloaded extension files",
+                    "\n".join([str(path.relative_to(dir_path)) for path in extension_filepaths])
+                )
+        return
 
     def _initialize_api_cache(self):
         self.logger.h3("Initialize Cache")
-        if not self._path_api_cache.is_file():
-            self.logger.info(f"API cache file not found at '{self._path_api_cache}'.")
+        if not self.path.local_api_cache.is_file():
+            self.logger.info(f"API cache file not found at '{self.path.local_api_cache}'.")
             cache = {}
             return cache
-        cache = self._read_yaml(self._path_api_cache)
-        self.logger.success(f"API cache loaded from '{self._path_api_cache}'", json.dumps(cache, indent=3))
+        cache = self._read_datafile(self.path.local_api_cache)
+        self.logger.success(f"API cache loaded from '{self.path.local_api_cache}'", json.dumps(cache, indent=3))
         return cache
 
     def _get_local_config(self):
-        local_config = {
-            "meta_cache_retention_days": {
-                "api": 1,
-                "extensions": 1
-            }
-        }
         self.logger.h3("Read Local Config")
-        path_local_config = self._path_local / "config.yaml"
-        if not path_local_config.is_file():
-            self.logger.attention(
-                f"Local config file '{path_local_config}' not found; setting default values.",
-                json.dumps(local_config, indent=3)
-            )
-            return local_config
-        local_config = local_config | self._read_yaml(
-            path_local_config,
-            schema=_util.file.datafile("schema_local_config.yaml")
+        local_config = self._read_datafile(
+            source=self.path.local_config,
+            schema=self._get_schema("local_config"),
         )
         self.logger.success("Local config set.", json.dumps(local_config, indent=3))
         return local_config
 
-    def _read_core_metadata(self) -> dict:
-        self.logger.h3("Read Core Metadata")
-        data_path = self._path_meta / "data"
-        if not data_path.is_dir():
-            self.logger.error(
-                f"Meta directory '{self._path_meta}' does not contain a 'data' directory"
-            )
-        metadata = self._read_data(self._path_meta)
-        if not metadata:
-            self.logger.error(f"No '.yaml' file found at {data_path}.")
-        return metadata
-
-    def _update_metadata_from_extensions(self) -> None:
-        self.logger.h3("Read Extended Metadata")
-        for idx, extension in enumerate(self._extensions):
-            if extension["type"] in ["meta", "data"]:
-                self.logger.h4(f"Read Extension Metadata {idx + 1}")
-                extension_metadata = self._read_data(self._path_extensions / f"{idx + 1 :03}")
-                if not extension_metadata:
-                    if extension["type"] == "data":
-                        self.logger.error(
-                            f"Extension '{extension}' has type 'data' but no metadata were found."
-                        )
-                    else:
-                        self.logger.skip(
-                            f"Extension '{extension}' with type 'meta' has no metadata."
-                        )
-                    continue
-                self._recursive_update(
-                    self._metadata,
-                    extension_metadata,
-                    append_list=extension.get('append_list', True),
-                    append_dict=extension.get('append_dict', True),
-                    raise_on_duplicated=extension.get('raise_duplicate', False),
-                )
-        return
-
-    def _add_metadata_default_values(self):
-        self.logger.h3("Set Metadata Missing Values")
-        defaults = self._read_yaml(_util.file.datafile("default_metadata.yaml"))
-        self._recursive_update(source=self._metadata, add=defaults, append_list=False)
-        self.logger.success("Full metadata file assembled.", json.dumps(self._metadata, indent=3))
-        return
-
-    def _validate_metadata(self):
-        self.logger.h3("Validate Metadata")
-        with open(_util.file.datafile("schema_metadata.json")) as f:
-            schema = json.load(f)
-        try:
-            jsonschema.validate(instance=self._metadata, schema=schema)
-        except jsonschema.exceptions.ValidationError as e:
-            self.logger.error(f"Invalid metadata schema: {e.message}.", traceback.format_exc())
-        self.logger.success("Metadata schema validated")
-        return
-
-    def _read_data(self, dirpath_meta: Path) -> dict:
-        """
-        Read metadata from a single 'meta' directory.
-
-        Parameters
-        ----------
-        dirpath_meta : Path
-            Path to the 'meta' directory containing the 'data' subdirectory with metadata files.
-        """
-        dirpath_data = Path(dirpath_meta) / "data"
-        metadata_files = list(dirpath_data.glob("*.yaml"))
-        metadata = dict()
-        for path_file in metadata_files:
-            section = self._read_yaml(path_file)
-            if not isinstance(section, dict):
-                self.logger.error(
-                    f"Invalid metadata file",
-                    f"Expected a dict, but '{path_file}' had:\n{json.dumps(section, indent=3)}"
-                )
-            for new_key in section:
-                if new_key in metadata:
-                    self.logger.error(
-                        f"Found a duplicate of metadata key '{new_key}' in '{path_file.name}'."
+    def _read_raw_metadata(self):
+        self.logger.h3("Read Raw Metadata")
+        data = [
+            "core/credits",
+            "core/intro",
+            "core/license",
+            "dev/branches",
+            "dev/changelogs",
+            "dev/commits",
+            "dev/issues",
+            "dev/labels",
+            "dev/maintainers",
+            "dev/repo",
+            "dev/tags",
+            "dev/workflows",
+            "package/metadata",
+            "ui/health_files",
+            "ui/readme",
+            "ui/theme",
+            "ui/web",
+        ]
+        metadata = {}
+        for entry in data:
+            section = self._read_datafile(self.path.dir_meta / f"{entry}.yaml")
+            for idx, extension in enumerate(self._extensions["extensions"]):
+                if extension["type"] == entry:
+                    self.logger.h4(f"Read Extension Metadata {idx + 1}")
+                    extionsion_path = self._path_extensions / f"{idx + 1 :03}" / f"{entry}.yaml"
+                    section_extension = self._read_datafile(extionsion_path, raise_missing=True)
+                    self._recursive_update(
+                        source=section,
+                        add=section_extension,
+                        append_list=extension['append_list'],
+                        append_dict=extension['append_dict'],
+                        raise_on_duplicated=extension['raise_duplicate']
                     )
+            self._validate_datafile(source=section, schema=entry)
+            shared_keys = set(metadata.keys()) & set(section.keys())
+            if shared_keys:
+                self.logger.error(
+                    f"Found duplicate metadata key(s) '{shared_keys}' in {entry}.yaml."
+                )
             metadata |= section
+        self.logger.success("Full metadata file assembled.", json.dumps(metadata, indent=3))
         return metadata
 
     def _read_package_config(self):
@@ -329,49 +310,15 @@ class MetaReader:
             )
         return final_config
 
-    def _download_extensions(self, extensions: list[dict], download_path: Path) -> None:
-        dest_path = {
-            "meta": ".",
-            "config": "config",
-            "pyproject": "config/pyproject",
-            "data": "data",
-            "template": "template",
-            "health_file": "template/health_file",
-            "license": "template/license",
-            "issue": "template/issue",
-            "discussion": "template/discussion",
-            "pull": "template/pull",
-            "media": "media",
-            "logo": "media/logo",
-        }
-        self.logger.h3("Download Meta Extensions")
-        _util.file.delete_dir_content(self._path_extensions_dir, exclude=["README.md"])
-        for idx, extension in enumerate(extensions):
-            self.logger.h4(f"Download Extension {idx + 1}")
-            self.logger.info(f"Input: {extension}")
-            repo_owner, repo_name = extension['repo'].split("/")
-            dir_path = download_path / f"{idx + 1 :03}"
-            try:
-                extension_filepaths = self.github.user(repo_owner).repo(repo_name).download_content(
-                    path=extension.get('path', ''),
-                    ref=extension.get('ref'),
-                    download_path=dir_path / dest_path[extension['type']],
-                    recursive=True,
-                    keep_full_path=False,
-                )
-            except WebAPIPersistentStatusCodeError as e:
-                self.logger.error(f"Error downloading extension data:", str(e))
-            if not extension_filepaths:
-                self.logger.error(f"No files found in extension.")
-            else:
-                self.logger.success(
-                    f"Downloaded extension files",
-                    "\n".join([str(path.relative_to(dir_path)) for path in extension_filepaths])
-                )
-        return
+    def _read_datafile(self, source: Path, schema: Path = None, **kwargs):
+        return _util.dict.read(path=source, schema=schema, logger=self.logger, **kwargs)
 
-    def _read_yaml(self, source: Path, schema: Path = None):
-        return _util.dict.read(path=source, schema=schema, logger=self.logger)
+    def _validate_datafile(self, source: dict, schema: str):
+        return _util.dict.validate_schema(source=source, schema=self._get_schema(schema), logger=self.logger)
+
+    @staticmethod
+    def _get_schema(schema: str):
+        return _util.file.datafile(f"schema/{schema}.yaml")
 
     def _is_expired(self, timestamp: str, typ: Literal['api', 'extensions'] = 'api') -> bool:
         exp_date = datetime.datetime.strptime(timestamp, "%Y_%m_%d_%H_%M_%S") + datetime.timedelta(
@@ -402,3 +349,74 @@ class MetaReader:
             logger=self.logger
         )
         return
+
+
+class PathReader:
+    def __init__(self, path_root: str | Path, logger: Logger | None = None):
+        self._logger = logger or Logger()
+        self._path_root = Path(path_root).resolve()
+        self._paths = _util.dict.read(
+            path=self._path_root / ".path.json",
+            schema=_util.file.datafile("schema/path.yaml"),
+            logger=self._logger
+        )
+
+        if not self.dir_meta.is_dir():
+            self._logger.error(f"Input meta directory '{self.dir_meta}' not found.")
+
+        if not self.dir_local_meta_extensions.is_dir():
+            self._logger.error(
+                f"Could not find Local meta extensions directory at: '{self.dir_local_meta_extensions}'."
+            )
+        return
+
+    @property
+    def paths(self) -> dict:
+        return self._paths
+
+    @property
+    def root(self):
+        return self._path_root
+
+    @property
+    def dir_source(self):
+        return self._path_root / self._paths["dir"]["source"]
+
+    @property
+    def dir_meta(self):
+        return self._path_root / self._paths["dir"]["meta"]
+
+    @property
+    def dir_local(self):
+        return self._path_root / self._paths["dir"]["local"]
+
+    @property
+    def dir_local_meta(self):
+        return self.dir_local / "meta"
+
+    @property
+    def dir_local_meta_extensions(self):
+        return self.dir_local_meta / "extensions"
+
+    @property
+    def dir_meta_package_config_build(self):
+        return self.dir_meta / "package" / "config_build"
+
+    @property
+    def dir_meta_package_config_tools(self):
+        return self.dir_meta / "package" / "config_tools"
+
+    @property
+    def local_config(self) -> Path:
+        return self._path_root / self._paths["file"]["meta"]["config_local"]
+
+    @property
+    def local_api_cache(self):
+        return self.dir_local_meta / "api_cache.yaml"
+
+    @property
+    def meta_core_extensions(self):
+        return self.dir_meta / "core" / "extensions.yaml"
+
+
+
