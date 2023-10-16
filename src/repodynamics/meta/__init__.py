8b0a6efcdcb57940206032e14b00a9f79f51417a
AAriam <80158628+AAriam@users.noreply.github.com>
2023-10-16 06:53:06 +0200

    Update __init__.py

--- src/repodynamics/meta/__init__.py
+++ src/repodynamics/meta/__init__.py
@@ -18,39 +18,9 @@ def update(
     github_token: Optional[str] = None,
     logger: Logger = None
 ) -> dict:
-    logger = logger or Logger()
-    reader = MetaReader(
-        path_root=path_root,
-        github_token=github_token,
-        logger=logger
-    )
-    metadata_gen = MetadataGenerator(
-        reader=reader,
-    )
-    metadata = metadata_gen.generate()
-    generated_files = files.generate(metadata=metadata, reader=reader, logger=logger)
+
+
     writer = MetaWriter(path_root=path_root, logger=logger)
     output = writer.write(generated_files, action=action)
     output['metadata'] = metadata
     return output
-
-
-def load(
-    path_root: str | Path = ".",
-    path_metadata: str = ".meta/.metadata.json",
-    logger: Logger = None
-) -> dict:
-    logger = logger or Logger()
-    path_metadata = Path(path_root) / path_metadata
-    metadata = _util.dict.read(path_metadata, logger=logger) or {}
-    if metadata:
-        logger.success(
-            f"Loaded metadata from {path_metadata}.",
-            json.dumps(metadata, indent=3)
-        )
-    else:
-        logger.attention(f"No metadata found in {path_metadata}.")
-    defaults = _util.dict.read(_util.file.datafile("default_metadata.yaml"))
-    _util.dict.update_recursive(source=metadata, add=defaults, append_list=False, logger=logger)
-    logger.success("Full metadata file assembled.", json.dumps(metadata, indent=3))
-    return metadata
