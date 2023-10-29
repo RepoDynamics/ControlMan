1f62c1ecb9983952a0182a7b6a65030a6b5280e8
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-10-29 18:30:50 +0100

    Update init.py

--- src/repodynamics/actions/init.py
+++ src/repodynamics/actions/init.py
@@ -340,13 +340,16 @@ class Init:
         return
 
     def event_push_branch_modified_main(self):
+        self.event_type = EventType.PUSH_MAIN
         self.metadata_before = self.git.file_at_hash(
             commit_hash=self.hash_before, path=self.meta.output_path.metadata.rel_path
         )
+        self.action_repo_labels_sync()
+
         self.action_file_change_detector()
         for job_id in ("package_build", "package_test_local", "package_lint", "website_build"):
             self.set_job_run(job_id)
-        self.event_type = EventType.PUSH_MAIN
+
         self.action_meta()
         self.action_hooks()
         self.last_ver, self.dist_ver = self.get_latest_version()
@@ -403,6 +406,7 @@ class Init:
         return
 
     def event_push_branch_modified_release(self):
+        self.event_type = EventType.PUSH_RELEASE
         return
 
     def event_push_branch_modified_dev(self):
@@ -485,11 +489,13 @@ class Init:
         return
 
     def event_schedule_sync(self):
+        self.event_type = EventType.SCHEDULE
         self.action_website_announcement_check()
         self.action_meta()
         return
 
     def event_schedule_test(self):
+        self.event_type = EventType.SCHEDULE
         return
 
     def event_issues_opened(self):
@@ -862,13 +868,73 @@ class Init:
         name = "Repository Labels Synchronizer"
         self.logger.h1(name)
         current_labels = self.api.labels
+        new_labels = self.metadata["label"]["compiled"]
         if init:
             for label in current_labels:
                 self.api.label_delete(label["name"])
-            for label in self.metadata["label"]["list"]:
+            for label in new_labels.values():
                 self.api.label_create(**label)
             return
-
+        old_labels = self.metadata_before["label"]["compiled"]
+        old_labels_not_versions = {}
+        old_labels_versions = {}
+        for label_id in old_labels.keys():
+            if label_id[:2] == ("auto_group", "version"):
+                old_labels_versions[label_id[2]] = old_labels[label_id]
+            else:
+                old_labels_not_versions[label_id] = old_labels[label_id]
+        new_labels_versions = {}
+        new_labels_not_versions = {}
+        for label_id in new_labels.keys():
+            if label_id[:2] == ("auto_group", "version"):
+                new_labels_versions[label_id[2]] = new_labels[label_id]
+            else:
+                new_labels_not_versions[label_id] = new_labels[label_id]
+        old_ids = set(old_labels_not_versions.keys())
+        new_ids = set(new_labels_not_versions.keys())
+        deleted_ids = old_ids - new_ids
+        added_ids = new_ids - old_ids
+        added_version_ids = set(new_labels_versions.keys()) - set(old_labels_versions.keys())
+        deleted_version_ids = sorted(
+            [PEP440SemVer(ver) for ver in set(old_labels_versions.keys()) - set(new_labels_versions.keys())],
+            reverse=True
+        )
+        remaining_allowed_number = 1000 - len(new_labels)
+        still_allowed_version_ids = deleted_version_ids[:remaining_allowed_number]
+        outdated_version_ids = deleted_version_ids[remaining_allowed_number:]
+        for outdated_version_id in outdated_version_ids:
+            self.api.label_delete(old_labels_versions[str(outdated_version_id)]["name"])
+        for deleted_id in deleted_ids:
+            self.api.label_delete(old_labels[deleted_id]["name"])
+        for new_label_version_id in added_version_ids:
+            self.api.label_create(**new_labels_versions[new_label_version_id])
+        for added_id in added_ids:
+            self.api.label_create(**new_labels[added_id])
+        possibly_modified_ids = set(old_labels.keys()) & set(new_labels.keys())
+        for possibly_modified_id in possibly_modified_ids:
+            old_label = old_labels[possibly_modified_id]
+            new_label = new_labels[possibly_modified_id]
+            if old_label != new_label:
+                self.api.label_update(
+                    name=old_label["name"],
+                    new_name=new_label["name"],
+                    description=new_label["description"],
+                    color=new_label["color"]
+                )
+        if not still_allowed_version_ids:
+            return
+        if self.metadata_before["label"]["auto_group"]["version"] == self.metadata["label"]["auto_group"]["version"]:
+            return
+        new_prefix = self.metadata["label"]["auto_group"]["version"]["prefix"]
+        new_color = self.metadata["label"]["auto_group"]["version"]["color"]
+        new_description = self.metadata["label"]["auto_group"]["version"]["description"]
+        for still_allowed_version_id in still_allowed_version_ids:
+            self.api.label_update(
+                name=old_labels_versions[str(still_allowed_version_id)]["name"],
+                new_name=f"{new_prefix}{still_allowed_version_id}",
+                description=new_description,
+                color=new_color
+            )
         return
 
     def action_repo_settings_sync(self):
@@ -879,8 +945,20 @@ class Init:
             "squash_merge_commit_message": "PR_BODY",
         }
         topics = data.pop("topics")
-        self.api_admin.update_settings(settings=data)
-        self.api_admin.replace_topics(topics=topics)
+        self.api_admin.repo_update(**data)
+        self.api_admin.repo_topics_replace(topics=topics)
+        return
+
+    def action_repo_pages(self):
+        if not self.api.info["has_pages"]:
+            self.api_admin.pages_create(build_type="workflow")
+        custom_url = self.metadata["web"].get("base_url")
+        if custom_url:
+            cname = custom_url.removeprefix("https://").removeprefix("http://")
+            https_enforced = custom_url.startswith("https://")
+        else:
+            cname = https_enforced = None
+        self.api_admin.pages_update(cname=cname, https_enforced=https_enforced, build_type="workflow")
         return
 
     def action_website_announcement_check(self):
