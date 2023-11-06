03f8eeac11401dd048cd2f8b233bf91d36f2217c
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-11-06 03:37:35 +0100

    Update __init__.py

--- src/repodynamics/meta/__init__.py
+++ src/repodynamics/meta/__init__.py
@@ -2,23 +2,28 @@ from typing import Literal, Optional, Sequence, Callable
 from pathlib import Path
 import json
 
-from ruamel.yaml import YAML
-
 from repodynamics.logger import Logger
 from repodynamics.meta.metadata import MetadataGenerator
+from repodynamics.meta.manager import MetaManager
 from repodynamics.meta.reader import MetaReader
+from repodynamics.meta.validator import MetaValidator
 from repodynamics.meta import files
+from repodynamics.path import RelativePath
 from repodynamics.meta.writer import MetaWriter
 from repodynamics import _util
 
 
-def update(
-    path_root: str | Path = ".",
-    action: Literal["report", "apply", "amend", "commit"] = "report",
-    github_token: Optional[str] = None,
-    logger: Logger = None,
-) -> dict:
-    writer = MetaWriter(path_root=path_root, logger=logger)
-    output = writer.write(generated_files, action=action)
-    output["metadata"] = metadata
-    return output
+def read_from_json_file(path_root: str | Path = ".", logger: Logger | None = None) -> MetaManager:
+    logger = logger or Logger()
+    path_root = Path(path_root).resolve()
+    path_json = path_root / RelativePath.file_metadata
+    metadata = _util.dict.read(path_json, logger=logger, raise_empty=True)
+    MetaValidator(metadata=metadata, logger=logger).validate()
+    return MetaManager(metadata=metadata)
+
+
+def read_from_json_string(content: str, logger: Logger | None = None) -> MetaManager:
+    logger = logger or Logger()
+    metadata = json.loads(content)
+    MetaValidator(metadata=metadata, logger=logger).validate()
+    return MetaManager(metadata=metadata)
