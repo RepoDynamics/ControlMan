b56a21311d996fa11dde9939f4487bc103390a54
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-11-13 11:55:36 +0100

    Update package.py

--- src/repodynamics/meta/files/package.py
+++ src/repodynamics/meta/files/package.py
@@ -37,18 +37,20 @@ class PackageFileGenerator:
         self._pyproject_test = test_package_config
         self._out_db = output_path
 
-        self._package_dir_output = []
+        self._package_dir_output: tuple[DynamicFile, str] | None = None
+        self._test_package_dir_output: tuple[DynamicFile, str] | None = None
         return
 
     def generate(self):
-        self._package_dir_output = self._package_dir()
+        self._package_dir_output, self._test_package_dir_output = self._directories()
         return (
             self.requirements()
             + self.init_docstring()
             + self.pyproject()
             + self.pyproject_tests()
-            + self._package_dir_output
-            + self._package_dir(tests=True)
+            + [self._package_dir_output]
+            + [self._test_package_dir_output]
+            + self.python_files()
             + self.typing_marker()
             + self.manifest()
         )
@@ -75,57 +77,131 @@ class PackageFileGenerator:
                     text += f"{dep['pip_spec']}\n"
         return [(info, text)]
 
-    def _package_dir(self, tests: bool = False) -> list[tuple[DynamicFile, str]]:
+    def _directories(self) -> list[tuple[DynamicFile, str]]:
         self._logger.h4("Update path: package")
         package_name = self._meta["package"]["name"]
-        name = package_name if not tests else f"{package_name}_tests"
-        sub_path = (
-            self._meta["path"]["dir"]["source"] if not tests else f'{self._meta["path"]["dir"]["tests"]}/src'
-        )
-        path = self._out_db.root / sub_path / name
-        func = self._out_db.package_tests_dir if tests else self._out_db.package_dir
-        if path.exists():
-            self._logger.skip(f"Package path exists", f"{path}")
-            out = [(func(name, path, path), "")]
-            return out
-        self._logger.info(f"Package path '{path}' does not exist; looking for package directory.")
-        package_dirs = (
-            [
-                subdir
-                for subdir in [content for content in path.parent.iterdir() if content.is_dir()]
-                if "__init__.py"
-                in [sub_content.name for sub_content in subdir.iterdir() if sub_content.is_file()]
-            ]
-            if path.parent.is_dir()
-            else []
-        )
-        count_dirs = len(package_dirs)
-        if count_dirs > 1:
-            self._logger.error(
-                f"More than one package directory found in '{path}'",
-                "\n".join([str(package_dir) for package_dir in package_dirs]),
+        out = []
+        for name, sub_path, func in (
+            (package_name, self._meta["path"]["dir"]["source"], self._out_db.package_dir),
+            (
+                f"{package_name}_tests",
+                f'{self._meta["path"]["dir"]["tests"]}/src',
+                self._out_db.package_tests_dir
+            ),
+        ):
+            path = self._out_db.root / sub_path / name
+            if path.exists():
+                self._logger.skip(f"Package path exists", f"{path}")
+                out.append((func(path, path), ""))
+                continue
+            self._logger.info(f"Package path '{path}' does not exist; looking for package directory.")
+            package_dirs = (
+                [
+                    subdir
+                    for subdir in [content for content in path.parent.iterdir() if content.is_dir()]
+                    if "__init__.py"
+                    in [sub_content.name for sub_content in subdir.iterdir() if sub_content.is_file()]
+                ]
+                if path.parent.is_dir()
+                else []
             )
-        if count_dirs == 1:
-            self._logger.success(
-                f"Rename package directory to '{name}'",
-                f"Old Path: '{package_dirs[0]}'\nNew Path: '{path}'",
-            )
-            out = [(func(package_name, old_path=package_dirs[0], new_path=path), "")]
-            package_old_name = package_dirs[0].name
-            for filepath in (self._out_db.root.glob("**/*.py") if not tests else path.glob("**/*.py")):
-                new_content = self.rename_imports(
-                    module_content=filepath.read_text(), old_name=package_old_name, new_name=name
+            count_dirs = len(package_dirs)
+            if count_dirs > 1:
+                self._logger.error(
+                    f"More than one package directory found in '{path}'",
+                    "\n".join([str(package_dir) for package_dir in package_dirs]),
                 )
-                out.append((self._out_db.python_file(filepath), new_content))
-            return out
-        self._logger.success(f"No package directory found in '{path}'; creating one.")
-        out = [(func(name, old_path=None, new_path=path), "")]
-        if tests:
+            if count_dirs == 1:
+                self._logger.success(
+                    f"Rename package directory to '{name}'",
+                    f"Old Path: '{package_dirs[0]}'\nNew Path: '{path}'",
+                )
+                out.append((func(old_path=package_dirs[0], new_path=path), ""))
+                continue
+            self._logger.success(f"No package directory found in '{path}'; creating one.")
+            out.append((func(old_path=None, new_path=path), ""))
+        return out
+
+    def python_files(self) -> list[tuple[DynamicFile, str]]:
+        out = []
+        mapping = {}
+        package_dir = self._package_dir_output[0]
+        test_package_dir = self._test_package_dir_output[0]
+        for dir_ in (package_dir, test_package_dir):
+            if dir_.alt_paths and dir_.alt_paths[0] != dir_.path:
+                mapping[dir_.alt_paths[0].name] = dir_.path.name
+        if mapping:
+            for filepath in self._out_db.root.glob("**/*.py"):
+                if package_dir.alt_paths and filepath.is_relative_to(package_dir.alt_paths[0]):
+                    new_path = package_dir.path / filepath.relative_to(package_dir.alt_paths[0])
+                elif test_package_dir.alt_paths and filepath.is_relative_to(test_package_dir.alt_paths[0]):
+                    new_path = test_package_dir.path / filepath.relative_to(test_package_dir.alt_paths[0])
+                else:
+                    new_path = filepath
+                new_content = self.rename_imports(module_content=filepath.read_text(), mapping=mapping)
+                out.append((self._out_db.python_file(new_path), new_content))
+        if not test_package_dir.alt_paths:
+            # test-suite package must be created
             for testsuite_filename in ["__init__.txt", "__main__.txt", "general_tests.txt"]:
                 filepath = _util.file.datafile(f"template/testsuite/{testsuite_filename}")
                 text = _util.dict.fill_template(filepath.read_text(), metadata=self._meta.as_dict)
-                out.append((self._out_db.python_file((path / testsuite_filename).with_suffix(".py")), text))
+                path = self._out_db.python_file(
+                    (test_package_dir.path / testsuite_filename).with_suffix(".py")
+                )
+                out.append((path, text))
         return out
+
+    # def _package_dir(self, tests: bool = False) -> list[tuple[DynamicFile, str]]:
+    #     self._logger.h4("Update path: package")
+    #     package_name = self._meta["package"]["name"]
+    #     name = package_name if not tests else f"{package_name}_tests"
+    #     sub_path = (
+    #         self._meta["path"]["dir"]["source"] if not tests else f'{self._meta["path"]["dir"]["tests"]}/src'
+    #     )
+    #     path = self._out_db.root / sub_path / name
+    #     func = self._out_db.package_tests_dir if tests else self._out_db.package_dir
+    #     if path.exists():
+    #         self._logger.skip(f"Package path exists", f"{path}")
+    #         out = [(func(name, path, path), "")]
+    #         return out
+    #     self._logger.info(f"Package path '{path}' does not exist; looking for package directory.")
+    #     package_dirs = (
+    #         [
+    #             subdir
+    #             for subdir in [content for content in path.parent.iterdir() if content.is_dir()]
+    #             if "__init__.py"
+    #             in [sub_content.name for sub_content in subdir.iterdir() if sub_content.is_file()]
+    #         ]
+    #         if path.parent.is_dir()
+    #         else []
+    #     )
+    #     count_dirs = len(package_dirs)
+    #     if count_dirs > 1:
+    #         self._logger.error(
+    #             f"More than one package directory found in '{path}'",
+    #             "\n".join([str(package_dir) for package_dir in package_dirs]),
+    #         )
+    #     if count_dirs == 1:
+    #         self._logger.success(
+    #             f"Rename package directory to '{name}'",
+    #             f"Old Path: '{package_dirs[0]}'\nNew Path: '{path}'",
+    #         )
+    #         out = [(func(package_name, old_path=package_dirs[0], new_path=path), "")]
+    #         package_old_name = package_dirs[0].name
+    #         for filepath in (self._out_db.root.glob("**/*.py") if not tests else path.glob("**/*.py")):
+    #             new_content = self.rename_imports(
+    #                 module_content=filepath.read_text(), old_name=package_old_name, new_name=name
+    #             )
+    #             out.append((self._out_db.python_file(filepath), new_content))
+    #         return out
+    #     self._logger.success(f"No package directory found in '{path}'; creating one.")
+    #     out = [(func(name, old_path=None, new_path=path), "")]
+    #     if tests:
+    #         for testsuite_filename in ["__init__.txt", "__main__.txt", "general_tests.txt"]:
+    #             filepath = _util.file.datafile(f"template/testsuite/{testsuite_filename}")
+    #             text = _util.dict.fill_template(filepath.read_text(), metadata=self._meta.as_dict)
+    #             out.append((self._out_db.python_file((path / testsuite_filename).with_suffix(".py")), text))
+    #     return out
 
     def init_docstring(self) -> list[tuple[DynamicFile, str]]:
         self._logger.h3("Generate File Content: __init__.py")
@@ -140,7 +216,7 @@ class PackageFileGenerator:
         )
         docstring = f'"""{docstring_text}\n"""  # noqa: D400\n'
 
-        package_dir_info = self._package_dir_output[0][0]
+        package_dir_info = self._package_dir_output[0]
         current_dir_path = (
             package_dir_info.alt_paths[0] if package_dir_info.alt_paths else package_dir_info.path
         )
@@ -291,29 +367,35 @@ __version__ = __version_details__["version"]"""
             else None
         )
 
-    def rename_imports(self, module_content: str, old_name: str, new_name: str) -> str:
+    @staticmethod
+    def rename_imports(module_content: str, mapping: dict[str, str]) -> str:
         """
         Rename the old import name to the new import name in the provided module content.
 
-        Parameters:
-        - module_content: str - The content of the Python module as a string.
-        - old_name: str - The old name of the import to be renamed.
-        - new_name: str - The new name that will replace the old name.
+        Parameters
+        ----------
+        module_content : str
+            The content of the Python module as a string.
+        mapping : dict[str, str]
+            A dictionary mapping the old import names to the new import names.
 
-        Returns:
-        - The updated module content as a string with the old names replaced by the new names.
+        Returns
+        -------
+        new_module_content : str
+            The updated module content as a string with the old names replaced by the new names.
         """
-        # Regular expression patterns to match the old name in import statements
-        patterns = [
-            rf"^\s*from\s+{re.escape(old_name)}(?:.[a-zA-Z0-9_]+)*\s+import",
-            rf"^\s*import\s+{re.escape(old_name)}(?:.[a-zA-Z0-9_]+)*",
-        ]
         updated_module_content = module_content
-        for pattern in patterns:
-            # Compile the pattern into a regular expression object
-            regex = re.compile(pattern, flags=re.MULTILINE)
-            # Replace the old name with the new name wherever it matches
-            updated_module_content = regex.sub(
-                lambda match: match.group(0).replace(old_name, new_name, 1), updated_module_content
-            )
+        for old_name, new_name in mapping.items():
+            # Regular expression patterns to match the old name in import statements
+            patterns = [
+                rf"^\s*from\s+{re.escape(old_name)}(?:.[a-zA-Z0-9_]+)*\s+import",
+                rf"^\s*import\s+{re.escape(old_name)}(?:.[a-zA-Z0-9_]+)*",
+            ]
+            for pattern in patterns:
+                # Compile the pattern into a regular expression object
+                regex = re.compile(pattern, flags=re.MULTILINE)
+                # Replace the old name with the new name wherever it matches
+                updated_module_content = regex.sub(
+                    lambda match: match.group(0).replace(old_name, new_name, 1), updated_module_content
+                )
         return updated_module_content
