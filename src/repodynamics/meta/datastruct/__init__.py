5f9ac7e539d2c489515f2d9e0d236df5dd088e56
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-12-29 21:48:05 +0100

    Update __init__.py

--- src/repodynamics/meta/datastruct/__init__.py
+++ src/repodynamics/meta/datastruct/__init__.py
@@ -6,9 +6,13 @@ class ControlCenterOptions:
     def __init__(self, options: dict):
         self._options = options
         self._project = project.Project(options)
+        self._dev = dev.Dev(options)
         return
 
     @property
     def project(self) -> project.Project:
         return self._project
 
+    @property
+    def dev(self) -> dev.Dev:
+        return self._dev
