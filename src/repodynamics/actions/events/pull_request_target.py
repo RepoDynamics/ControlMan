fa784425b9e9eb221166b7032969285966be3bb0
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-12-30 22:12:53 +0100

    Update pull_request_target.py

--- src/repodynamics/actions/events/pull_request_target.py
+++ src/repodynamics/actions/events/pull_request_target.py
@@ -1,9 +1,18 @@
-class PullRequestTargetEventHandler:
+from github_contexts import GitHubContext
+from github_contexts.github.payloads.pull_request import PullRequestPayload
+from github_contexts.github.enums import ActionType
+
+from repodynamics.actions.events._base import EventHandler
+from repodynamics.logger import Logger
+from repodynamics.datatype import TemplateType
+
+
+class PullRequestTargetEventHandler(EventHandler):
 
     def __init__(
         self,
         template_type: TemplateType,
-        context_manager: ContextManager,
+        context_manager: GitHubContext,
         admin_token: str,
         path_root_self: str,
         path_root_fork: str | None = None,
@@ -17,18 +26,19 @@ class PullRequestTargetEventHandler:
             path_root_fork=path_root_fork,
             logger=logger
         )
-        self._payload: IssuesPayload = self._context.payload
+        self._payload: PullRequestPayload = self._context.event
         return
 
-    def run(self):
-        if action == WorkflowTriggeringAction.OPENED:
+    def run_event(self):
+        action = self._payload.action
+        if action == ActionType.OPENED:
             self.event_pull_target_opened()
-        elif action == WorkflowTriggeringAction.REOPENED:
+        elif action == ActionType.REOPENED:
             self.event_pull_target_reopened()
-        elif action == WorkflowTriggeringAction.SYNCHRONIZE:
+        elif action == ActionType.SYNCHRONIZE:
             self.event_pull_target_synchronize()
         else:
-            self.logger.error(action_err_msg, action_err_details)
+            self.error_unsupported_triggering_action()
 
     def event_pull_target_opened(self):
         return
