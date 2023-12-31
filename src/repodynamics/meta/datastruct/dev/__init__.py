5a24aef0e5850e98ea2a43809da15ce0f8466bd4
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-12-31 01:13:09 +0100

    Update __init__.py

--- src/repodynamics/meta/datastruct/dev/__init__.py
+++ src/repodynamics/meta/datastruct/dev/__init__.py
@@ -1,13 +1,19 @@
 from repodynamics.meta.datastruct.dev import branch
+from repodynamics.meta.datastruct.dev import label
 
 
 class Dev:
 
-    def __init__(self, options: dict):
-        self._options = options
-        self._branch = branch.Branch(options)
+    def __init__(self, settings: dict):
+        self._ccs = settings
+        self._branch = branch.Branch(settings)
+        self._label = label.Label(settings)
         return
 
     @property
     def branch(self) -> branch.Branch:
         return self._branch
+
+    @property
+    def label(self) -> label.Label:
+        return self._label
