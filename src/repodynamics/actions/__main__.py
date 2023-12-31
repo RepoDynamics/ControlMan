73cb1641aad97b1fbbbd776cd7b7e6288891662a
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2023-12-31 03:34:33 +0100

    move actions/_db:action_color to actions/__main__.py

--- src/repodynamics/actions/__main__.py
+++ src/repodynamics/actions/__main__.py
@@ -5,7 +5,15 @@ import traceback
 
 from repodynamics.actions import io
 from repodynamics.logger import Logger
-from repodynamics.actions._db import action_color
+
+
+action_color = {
+    "context": (255, 199, 6),
+    "meta": (200, 120, 255),
+    "announce": (106, 245, 120),
+    "hooks": (252, 189, 0),
+    "init": (79, 255, 15),
+}
 
 
 def main():
