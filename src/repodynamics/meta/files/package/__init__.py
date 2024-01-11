bb76ffa2888817080b7238470acc5ee39027454c
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-11 09:56:57 +0100

    Update __init__.py

--- src/repodynamics/meta/files/package/__init__.py
+++ src/repodynamics/meta/files/package/__init__.py
@@ -1,3 +1,4 @@
+from repodynamics.datatype import DynamicFile
 from repodynamics.meta.files.package.python import PythonPackageFileGenerator
 from repodynamics.meta.manager import MetaManager
 from repodynamics.path import PathFinder
@@ -8,7 +9,7 @@ def generate(
     metadata: MetaManager,
     paths: PathFinder,
     logger: Logger = None,
-):
+) -> list[tuple[DynamicFile, str]]:
     if metadata["package"]["type"] == "python":
         return PythonPackageFileGenerator(metadata=metadata, paths=paths, logger=logger).generate()
     else:
