1125d0df5b5be8513a4f500bdc97134bd689f7fe
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-13 13:19:27 +0100

    Update pull_request.py

--- src/repodynamics/actions/events/pull_request.py
+++ src/repodynamics/actions/events/pull_request.py
@@ -119,7 +119,7 @@ class PullRequestEventHandler(EventHandler):
                 self.add_summary(name=name, status="fail", oneliner=error_title, details=error_msg)
                 return
 
-        action = self._context.event.action
+        action = self._payload.action
         if action is ActionType.OPENED:
             self._run_action_opened()
         elif action is ActionType.REOPENED:
@@ -397,7 +397,7 @@ class PullRequestEventHandler(EventHandler):
             )
             return
         hash_base = self._git_base.commit_hash_normal()
-        ver_base, dist_base = self._get_latest_version(head=False)
+        ver_base, dist_base = self._get_latest_version(base=False)
         labels = self._pull.label_names
         primary_commit_type = self._ccm_main.get_issue_data_from_labels(labels).group_data
         if primary_commit_type.group == CommitGroup.PRIMARY_CUSTOM or primary_commit_type.action in (
@@ -494,7 +494,7 @@ class PullRequestEventHandler(EventHandler):
             package_publish_pypi=True,
             github_release=True,
         )
-        self._set_release(
+        self._set_output_release(
             name=f"{self._ccm_main['name']} v{next_ver}",
             body=changelog_manager.get_entry(changelog_id="package_public")[0],
         )
@@ -537,69 +537,69 @@ class PullRequestEventHandler(EventHandler):
         )
         return
 
-    def event_pull_request(self):
-        self.event_type = EventType.PULL_MAIN
-        branch = self.resolve_branch(self.pull_head_ref_name)
-        if branch.type == BranchType.DEV and branch.suffix == 0:
-            return
-        for job_id in ("package_build", "package_test_local", "package_lint", "website_build"):
-            self.set_job_run(job_id)
-        self.git.checkout(branch=self.pull_base_ref_name)
-        latest_base_hash = self.git.commit_hash_normal()
-        base_ver, dist = self._get_latest_version()
-        self.git.checkout(branch=self.pull_head_ref_name)
-
-        self.action_file_change_detector()
-        self.action_meta()
-        self._action_hooks()
-
-        branch = self.resolve_branch(self.pull_head_ref_name)
-        issue_labels = [label["name"] for label in self.gh_api.issue_labels(number=branch.suffix)]
-        issue_data = self._ccm_main.get_issue_data_from_labels(issue_labels)
-
-        if issue_data.group_data.group == CommitGroup.PRIMARY_CUSTOM or issue_data.group_data.action in [
-            PrimaryActionCommitType.WEBSITE,
-            PrimaryActionCommitType.META,
-        ]:
-            ver_dist = f"{base_ver}+{dist+1}"
-        else:
-            ver_dist = str(self._get_next_version(base_ver, issue_data.group_data.action))
-
-        changelog_manager = ChangelogManager(
-            changelog_metadata=self.metadata_main["changelog"],
-            ver_dist=ver_dist,
-            commit_type=issue_data.group_data.conv_type,
-            commit_title=self.pull_title,
-            parent_commit_hash=latest_base_hash,
-            parent_commit_url=self._gh_link.commit(latest_base_hash),
-            path_root=self._path_root_base,
-            logger=self.logger,
-        )
-
-        commits = self._get_commits()
-        self.logger.success(f"Found {len(commits)} commits.")
-        for commit in commits:
-            self.logger.info(f"Processing commit: {commit}")
-            if commit.group_data.group == CommitGroup.SECONDARY_CUSTOM:
-                changelog_manager.add_change(
-                    changelog_id=commit.group_data.changelog_id,
-                    section_id=commit.group_data.changelog_section_id,
-                    change_title=commit.msg.title,
-                    change_details=commit.msg.body,
-                )
-        entries = changelog_manager.get_all_entries()
-        self.logger.success(f"Found {len(entries)} changelog entries.", str(entries))
-        curr_body = self.pull_body.strip() if self.pull_body else ""
-        if curr_body:
-            curr_body += "\n\n"
-        for entry, changelog_name in entries:
-            curr_body += f"# Changelog: {changelog_name}\n\n{entry}\n\n"
-        self.gh_api.pull_update(
-            number=self.pull_number,
-            title=f"{issue_data.group_data.conv_type}: {self.pull_title.removeprefix(f'{issue_data.group_data.conv_type}: ')}",
-            body=curr_body,
-        )
-        return
+    # def event_pull_request(self):
+    #     self.event_type = EventType.PULL_MAIN
+    #     branch = self.resolve_branch(self.pull_head_ref_name)
+    #     if branch.type == BranchType.DEV and branch.suffix == 0:
+    #         return
+    #     for job_id in ("package_build", "package_test_local", "package_lint", "website_build"):
+    #         self.set_job_run(job_id)
+    #     self.git.checkout(branch=self.pull_base_ref_name)
+    #     latest_base_hash = self.git.commit_hash_normal()
+    #     base_ver, dist = self._get_latest_version()
+    #     self.git.checkout(branch=self.pull_head_ref_name)
+    #
+    #     self.action_file_change_detector()
+    #     self.action_meta()
+    #     self._action_hooks()
+    #
+    #     branch = self.resolve_branch(self.pull_head_ref_name)
+    #     issue_labels = [label["name"] for label in self.gh_api.issue_labels(number=branch.suffix)]
+    #     issue_data = self._ccm_main.get_issue_data_from_labels(issue_labels)
+    #
+    #     if issue_data.group_data.group == CommitGroup.PRIMARY_CUSTOM or issue_data.group_data.action in [
+    #         PrimaryActionCommitType.WEBSITE,
+    #         PrimaryActionCommitType.META,
+    #     ]:
+    #         ver_dist = f"{base_ver}+{dist+1}"
+    #     else:
+    #         ver_dist = str(self._get_next_version(base_ver, issue_data.group_data.action))
+    #
+    #     changelog_manager = ChangelogManager(
+    #         changelog_metadata=self.metadata_main["changelog"],
+    #         ver_dist=ver_dist,
+    #         commit_type=issue_data.group_data.conv_type,
+    #         commit_title=self.pull_title,
+    #         parent_commit_hash=latest_base_hash,
+    #         parent_commit_url=self._gh_link.commit(latest_base_hash),
+    #         path_root=self._path_root_base,
+    #         logger=self.logger,
+    #     )
+    #
+    #     commits = self._get_commits()
+    #     self.logger.success(f"Found {len(commits)} commits.")
+    #     for commit in commits:
+    #         self.logger.info(f"Processing commit: {commit}")
+    #         if commit.group_data.group == CommitGroup.SECONDARY_CUSTOM:
+    #             changelog_manager.add_change(
+    #                 changelog_id=commit.group_data.changelog_id,
+    #                 section_id=commit.group_data.changelog_section_id,
+    #                 change_title=commit.msg.title,
+    #                 change_details=commit.msg.body,
+    #             )
+    #     entries = changelog_manager.get_all_entries()
+    #     self.logger.success(f"Found {len(entries)} changelog entries.", str(entries))
+    #     curr_body = self.pull_body.strip() if self.pull_body else ""
+    #     if curr_body:
+    #         curr_body += "\n\n"
+    #     for entry, changelog_name in entries:
+    #         curr_body += f"# Changelog: {changelog_name}\n\n{entry}\n\n"
+    #     self.gh_api.pull_update(
+    #         number=self.pull_number,
+    #         title=f"{issue_data.group_data.conv_type}: {self.pull_title.removeprefix(f'{issue_data.group_data.conv_type}: ')}",
+    #         body=curr_body,
+    #     )
+    #     return
 
     def _update_implementation_tasklist(self) -> bool:
 
@@ -662,4 +662,3 @@ class PullRequestEventHandler(EventHandler):
             body=new_body,
         )
         return
-
