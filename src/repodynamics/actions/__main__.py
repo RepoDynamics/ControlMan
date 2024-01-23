ec34cb03660d52eba7fa5ec51c16b194b7b93cd4
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-24 00:02:20 +0100

    use package `actionman.io`

--- src/repodynamics/actions/__main__.py
+++ src/repodynamics/actions/__main__.py
@@ -3,9 +3,10 @@ import importlib
 import sys
 import traceback
 
-from repodynamics.actions import io
-from repodynamics.logger import Logger
+import actionman
 
+from repodynamics.logger import Logger
+from repodynamics.actions.init import init
 
 action_color = {
     "context": (255, 199, 6),
@@ -17,32 +18,38 @@ action_color = {
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("action", type=str, nargs="+", help="Name of the action to run.")
-    action = parser.parse_args().action
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("action", type=str, nargs="+", help="Name of the action to run.")
+    # action = parser.parse_args().action
     logger = Logger("github")
-    if len(action) > 2:
-        logger.error(f"Expected 2 arguments, but got {action}")
-    if len(action) == 1:
-        action.append(action[0])
-    action = [arg.replace("-", "_") for arg in action]
-    module_name, function_name = action
-    logger.success("Determine Action", [f"- Module: {module_name}", f"- Function: {function_name}"])
-    action_module = importlib.import_module(f"repodynamics.actions.{module_name}")
-    action = getattr(action_module, function_name)
-    inputs = io.input(module_name=module_name, function=action, logger=logger)
+    # if len(action) > 2:
+    #     logger.error(f"Expected 2 arguments, but got {action}")
+    # if len(action) == 1:
+    #     action.append(action[0])
+    # action = [arg.replace("-", "_") for arg in action]
+    # module_name, function_name = action
+    # logger.success("Determine Action", [f"- Module: {module_name}", f"- Function: {function_name}"])
+    # action_module = importlib.import_module(f"repodynamics.actions.{module_name}")
+    # action = getattr(action_module, function_name)
+    inputs = actionman.io.read_function_args_from_environment_variables(
+        function=init,
+        name_prefix="RD_INIT__",
+        hide_args=["admin_token"],
+        ignore_params=["logger"],
+        logger=logger
+    )
     try:
-        outputs, env_vars, summary = action(logger=logger, **inputs)
+        outputs, env_vars, summary = init(**inputs, logger=logger)
     except Exception as e:
         sys.stdout.flush()  # Flush stdout buffer before printing the exception
         logger.error(f"An unexpected error occurred: {e}", traceback.format_exc())
     logger.h1("Write Outputs & Summary")
     if outputs:
-        io.output(outputs, logger=logger)
+        actionman.io.write_github_outputs(outputs, logger=logger)
     if env_vars:
-        io.output(env_vars, env=True, logger=logger)
+        actionman.io.write_github_outputs(env_vars, to_env=True, logger=logger)
     if summary:
-        io.summary(content=summary, logger=logger)
+        actionman.io.write_github_summary(content=summary, logger=logger)
     return
 
 
