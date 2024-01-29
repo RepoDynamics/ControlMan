8c9a45fb240bab4f2268028443bf8acff7befbf0
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-29 08:13:44 +0100

    Update __init__.py

--- src/repodynamics/control/files/generator/__init__.py
+++ src/repodynamics/control/files/generator/__init__.py
@@ -1,5 +1,4 @@
-import json
-
+import pyserials as _pyserials
 from actionman.logger import Logger as _Logger
 
 from repodynamics.path import PathManager as _PathManager
@@ -21,16 +20,47 @@ def generate(
 ) -> list[tuple[_DynamicFile, str]]:
     logger.section("Generate Dynamic Repository Files", group=True)
     generated_files = []
-    logger.section("Generate Metadata")
-
-        # (path_manager.metadata, json.dumps(content_manager.as_dict)),
-        # (path_manager.license, content_manager["license"].get("text", "")),
-
-    for generator in (_config, _forms, _health, _package, _readme):
-        generated_files += generator.generate(
+    for generator in (
+        _generate_metadata,
+        _generate_license,
+        _config.generate,
+        _forms.generate,
+        _health.generate,
+        _package.generate,
+        _readme.generate,
+    ):
+        generated_files += generator(
             content_manager=content_manager,
             path_manager=path_manager,
             logger=logger,
         )
     logger.section_end()
     return generated_files
+
+
+def _generate_metadata(
+    content_manager: _ControlCenterContentManager,
+    path_manager: _PathManager,
+    logger: _Logger,
+) -> list[tuple[_DynamicFile, str]]:
+    logger.section("Generate Metadata File", group=True)
+    file_info = path_manager.metadata
+    file_content = _pyserials.write.to_json_string(data=content_manager.as_dict, sort_keys=True, indent=None)
+    logger.info(message="File Info:", code=str(file_info))
+    logger.debug(message="File content:", code=file_content)
+    logger.section_end()
+    return [(file_info, file_content)]
+
+
+def _generate_license(
+    content_manager: _ControlCenterContentManager,
+    path_manager: _PathManager,
+    logger: _Logger,
+) -> list[tuple[_DynamicFile, str]]:
+    logger.section("Generate License File", group=True)
+    file_info = path_manager.license
+    file_content = content_manager["license"].get("text", "")
+    logger.info(message="File Info:", code=str(file_info))
+    logger.debug(message="File content:", code=file_content)
+    logger.section_end()
+    return [(file_info, file_content)]
