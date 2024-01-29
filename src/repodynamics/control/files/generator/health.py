4800ea405ba7e8a1863051ddcac6e7043d5e76d5
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-29 07:40:12 +0100

    Update health.py

--- src/repodynamics/control/files/generator/health.py
+++ src/repodynamics/control/files/generator/health.py
@@ -6,22 +6,27 @@ from repodynamics.control.content import ControlCenterContentManager
 
 
 class HealthFileGenerator:
-    def __init__(self, metadata: ControlCenterContentManager, output_path: PathManager, logger: Logger = None):
+    def __init__(
+        self,
+        content_manager: ControlCenterContentManager,
+        path_manager: PathManager,
+        logger: Logger
+    ):
+        self._ccm = content_manager
+        self._pathman = path_manager
         self._logger = logger or Logger()
-        self._meta = metadata
-        self._out_db = output_path
         self._logger.h2("Generate Files")
         return
 
     def generate(self) -> list[tuple[DynamicFile, str]]:
         updates = []
-        for health_file_id, data in self._meta["health_file"].items():
-            info = self._out_db.health_file(health_file_id, target_path=data["path"])
-            text = self._generate_codeowners() if health_file_id == "codeowners" else data["text"]
+        for health_file_id, data in self._ccm["health_file"].items():
+            info = self._pathman.health_file(health_file_id, target_path=data["path"])
+            text = self._codeowners() if health_file_id == "codeowners" else data["text"]
             updates.append((info, text))
         return updates
 
-    def _generate_codeowners(self) -> str:
+    def _codeowners(self) -> str:
         """
 
         Returns
@@ -31,7 +36,7 @@ class HealthFileGenerator:
         ----------
         https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners#codeowners-syntax
         """
-        codeowners = self._meta["maintainer"].get("pull", {}).get("reviewer", {}).get("by_path")
+        codeowners = self._ccm["maintainer"].get("pull", {}).get("reviewer", {}).get("by_path")
         if not codeowners:
             return ""
         # Get the maximum length of patterns to align the columns when writing the file
