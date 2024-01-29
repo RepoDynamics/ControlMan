577189edd67ddb792626ba37566837dd55dd7dfb
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-29 07:56:13 +0100

    Update __init__.py

--- src/repodynamics/control/files/generator/__init__.py
+++ src/repodynamics/control/files/generator/__init__.py
@@ -1,49 +1,36 @@
 import json
 
-from actionman.logger import Logger
+from actionman.logger import Logger as _Logger
 
-from repodynamics.path import PathManager
-from repodynamics.datatype import DynamicFile
-from repodynamics.control.content import ControlCenterContentManager
-from repodynamics.control.files.generator import package, readme
-from repodynamics.control.files.generator.forms import FormGenerator
-from repodynamics.control.files.generator.config import ConfigFileGenerator
-from repodynamics.control.files.generator.health import HealthFileGenerator
+from repodynamics.path import PathManager as _PathManager
+from repodynamics.datatype import DynamicFile as _DynamicFile
+from repodynamics.control.content import ControlCenterContentManager as _ControlCenterContentManager
+from repodynamics.control.files.generator import (
+    config as _config,
+    forms as _forms,
+    health as _health,
+    package as _package,
+    readme as _readme,
+)
 
 
 def generate(
-    content_manager: ControlCenterContentManager,
-    path_manager: PathManager,
-    logger: Logger,
-) -> list[tuple[DynamicFile, str]]:
+    content_manager: _ControlCenterContentManager,
+    path_manager: _PathManager,
+    logger: _Logger,
+) -> list[tuple[_DynamicFile, str]]:
     logger.section("Generate Dynamic Repository Files", group=True)
-
     generated_files = []
-
     logger.section("Generate Metadata")
 
-        (path_manager.metadata, json.dumps(content_manager.as_dict)),
-        (path_manager.license, content_manager["license"].get("text", "")),
+        # (path_manager.metadata, json.dumps(content_manager.as_dict)),
+        # (path_manager.license, content_manager["license"].get("text", "")),
 
-
-    generated_files += ConfigFileGenerator(
-        content_manager=content_manager, path_manager=path_manager, logger=logger
-    ).generate()
-
-    generated_files += FormGenerator(
-        metadata=content_manager, output_path=path_manager, logger=logger
-    ).generate()
-
-    generated_files += HealthFileGenerator(
-        content_manager=content_manager, path_manager=path_manager, logger=logger
-    ).generate()
-
-    generated_files += package.generate(
-        metadata=content_manager,
-        paths=path_manager,
-        logger=logger,
-    )
-
-    generated_files += readme.generate(ccm=content_manager, path=path_manager, logger=logger)
+    for generator in (_config, _forms, _health, _package, _readme):
+        generated_files += generator.generate(
+            content_manager=content_manager,
+            path_manager=path_manager,
+            logger=logger,
+        )
     logger.section_end()
     return generated_files
