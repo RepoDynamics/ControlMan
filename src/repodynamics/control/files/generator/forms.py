64a77845c57b973fb7935430c11d77f3f1ed9676
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-30 01:55:42 +0100

    Update forms.py

--- src/repodynamics/control/files/generator/forms.py
+++ src/repodynamics/control/files/generator/forms.py
@@ -25,12 +25,15 @@ class _FormGenerator:
         return self.issue_forms() + self.discussion_forms() + self.pull_request_templates()
 
     def issue_forms(self) -> list[tuple[DynamicFile, str]]:
+        self._logger.section("Issue Forms", group=True)
         out = []
         issues = self._ccm["issue"]["forms"]
         issue_maintainers = self._ccm["maintainer"].get("issue", {})
         paths = []
         label_meta = self._ccm["label"]["group"]
         for idx, issue in enumerate(issues):
+            self._logger.section(f"Issue Form {idx + 1}")
+            file_info = self._pathman.issue_form(issue["id"], idx + 1)
             pre_process = issue.get("pre_process")
             if pre_process and not self._pre_process_existence(pre_process):
                 continue
@@ -62,47 +65,80 @@ class _FormGenerator:
                 form["body"].append(
                     {key: val for key, val in elem.items() if key not in ["pre_process", "post_process"]}
                 )
-            text = pyserials.write.to_yaml_string(data=form, end_of_file_newline=True)
-            info = self._pathman.issue_form(issue["id"], idx + 1)
-            out.append((info, text))
-            paths.append(info.path)
+            file_content = pyserials.write.to_yaml_string(data=form, end_of_file_newline=True)
+            out.append((file_info, file_content))
+            paths.append(file_info.path)
+            self._logger.info(message="File info:", code=str(file_info))
+            self._logger.debug(message="File content:", code=file_content)
+            self._logger.section_end()
         dir_issues = self._pathman.dir_issue_forms
         path_template_chooser = self._pathman.issue_template_chooser_config.path
         if dir_issues.is_dir():
             for file in dir_issues.glob("*.yaml"):
                 if file not in paths and file != path_template_chooser:
-                    out.append((self._pathman.issue_form_outdated(path=file), ""))
+                    self._logger.section(f"Outdated Issue Form: {file.name}")
+                    file_info = self._pathman.issue_form_outdated(path=file)
+                    file_content = ""
+                    out.append((file_info, file_content))
+                    self._logger.info(message="File info:", code=str(file_info))
+                    self._logger.debug(message="File content:", code=file_content)
+                    self._logger.section_end()
+        self._logger.section_end()
         return out
 
     def discussion_forms(self) -> list[tuple[DynamicFile, str]]:
+        self._logger.section("Discussion Forms", group=True)
         out = []
         paths = []
         forms = self._ccm["discussion"]["form"]
         for slug, form in forms.items():
-            info = self._pathman.discussion_form(slug)
-            text = pyserials.write.to_yaml_string(data=form, end_of_file_newline=True)
-            out.append((info, text))
-            paths.append(info.path)
+            self._logger.section(f"Discussion Form '{slug}'")
+            file_info = self._pathman.discussion_form(slug)
+            file_content = pyserials.write.to_yaml_string(data=form, end_of_file_newline=True)
+            out.append((file_info, file_content))
+            paths.append(file_info.path)
+            self._logger.info(message="File info:", code=str(file_info))
+            self._logger.debug(message="File content:", code=file_content)
+            self._logger.section_end()
         dir_discussions = self._pathman.dir_discussion_forms
         if dir_discussions.is_dir():
             for file in dir_discussions.glob("*.yaml"):
                 if file not in paths:
-                    out.append((self._pathman.discussion_form_outdated(path=file), ""))
+                    self._logger.section(f"Outdated Discussion Form: {file.name}")
+                    file_info = self._pathman.discussion_form_outdated(path=file)
+                    file_content = ""
+                    out.append((file_info, file_content))
+                    self._logger.info(message="File info:", code=str(file_info))
+                    self._logger.debug(message="File content:", code=file_content)
+                    self._logger.section_end()
+        self._logger.section_end()
         return out
 
     def pull_request_templates(self) -> list[tuple[DynamicFile, str]]:
+        self._logger.section("Pull Request Templates", group=True)
         out = []
         paths = []
         templates = self._ccm["pull"]["template"]
-        for name, text in templates.items():
-            info = self._pathman.pull_request_template(name=name)
-            out.append((info, text))
-            paths.append(info.path)
+        for name, file_content in templates.items():
+            self._logger.section(f"Template '{name}'")
+            file_info = self._pathman.pull_request_template(name=name)
+            out.append((file_info, file_content))
+            paths.append(file_info.path)
+            self._logger.info(message="File info:", code=str(file_info))
+            self._logger.debug(message="File content:", code=file_content)
+            self._logger.section_end()
         dir_templates = self._pathman.dir_pull_request_templates
         if dir_templates.is_dir():
             for file in dir_templates.glob("*.md"):
                 if file not in paths and file.name != "README.md":
-                    out.append((self._pathman.pull_request_template_outdated(path=file), ""))
+                    self._logger.section(f"Outdated Template: {file.name}")
+                    file_info = self._pathman.pull_request_template_outdated(path=file)
+                    file_content = ""
+                    out.append((file_info, file_content))
+                    self._logger.info(message="File info:", code=str(file_info))
+                    self._logger.debug(message="File content:", code=file_content)
+                    self._logger.section_end()
+        self._logger.section_end()
         return out
 
     @staticmethod
