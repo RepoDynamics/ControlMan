a065d0ca791081b9dab73f8eb2e497dc6f661413
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-30 01:45:04 +0100

    Update config.py

--- src/repodynamics/control/files/generator/config.py
+++ src/repodynamics/control/files/generator/config.py
@@ -30,7 +30,6 @@ class ConfigFileGenerator:
         self._ccm = content_manager
         self._path_manager = path_manager
         self._logger = logger
-        self._logger.h2("Generate Files")
         return
 
     def generate(self) -> list[tuple[DynamicFile, str]]:
@@ -51,93 +50,120 @@ class ConfigFileGenerator:
         ----------
         https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/displaying-a-sponsor-button-in-your-repository#about-funding-files
         """
-        self._logger.h3("Generate File: FUNDING.yml")
-        info = self._path_manager.funding
+        self._logger.section("GitHub Funding Configuration File")
+        file_info = self._path_manager.funding
         funding = self._ccm["funding"]
         if not funding:
-            self._logger.skip("'funding' not set in metadata; skipping.")
-            return [(info, "")]
-        output = {}
-        for funding_platform, users in funding.items():
-            if funding_platform in ["github", "custom"]:
-                if isinstance(users, list):
-                    output[funding_platform] = pyserials.format.to_yaml_array(data=users, inline=True)
-                elif isinstance(users, str):
+            file_content = ""
+        else:
+            output = {}
+            for funding_platform, users in funding.items():
+                if funding_platform in ["github", "custom"]:
+                    if isinstance(users, list):
+                        output[funding_platform] = pyserials.format.to_yaml_array(data=users, inline=True)
+                    elif isinstance(users, str):
+                        output[funding_platform] = users
+                    # Other cases are not possible because of the schema
+                else:
                     output[funding_platform] = users
-                # Other cases are not possible because of the schema
-            else:
-                output[funding_platform] = users
-        output_str = pyserials.write.to_yaml_string(data=output, end_of_file_newline=True)
-        self._logger.success(f"Generated 'FUNDING.yml' file.", output_str)
-        return [(info, output_str)]
+            file_content = pyserials.write.to_yaml_string(data=output, end_of_file_newline=True)
+        self._logger.info(message="File info:", code=str(file_info))
+        self._logger.debug(message="File content:", code=file_content)
+        self._logger.section_end()
+        return [(file_info, file_content)]
 
     def workflow_requirements(self) -> list[tuple[DynamicFile, str]]:
+        self._logger.section("Workflow Requirements Files")
         tools = self._ccm["workflow"]["tool"]
         out = []
         for tool_name, tool_spec in tools.items():
-            text = "\n".join(tool_spec["pip_spec"])
-            out.append((self._path_manager.workflow_requirements(tool_name), text))
+            self._logger.section(f"Tool: {tool_name}")
+            file_info = self._path_manager.workflow_requirements(tool_name)
+            file_content = "\n".join(tool_spec["pip_spec"])
+            out.append((file_info, file_content))
+            self._logger.info(message="File info:", code=str(file_info))
+            self._logger.debug(message="File content:", code=file_content)
+            self._logger.section_end()
+        self._logger.section_end()
         return out
 
     def pre_commit_config(self) -> list[tuple[DynamicFile, str]]:
+        self._logger.section("Pre-Commit Configuration Files")
         out = []
         for config_type in ("main", "release", "pre-release", "implementation", "development", "auto-update", "other"):
-            info = self._path_manager.pre_commit_config(config_type)
+            self._logger.section(f"Branch Type '{config_type}'")
+            file_info = self._path_manager.pre_commit_config(config_type)
             config = self._ccm["workflow"]["pre_commit"].get(config_type)
             if not config:
-                self._logger.skip("'pre_commit' not set in metadata.")
-                out.append((info, ""))
+                file_content = ""
             else:
-                text = pyserials.write.to_yaml_string(data=config, end_of_file_newline=True)
-                out.append((info, text))
+                file_content = pyserials.write.to_yaml_string(data=config, end_of_file_newline=True)
+            out.append((file_info, file_content))
+            self._logger.info(message="File info:", code=str(file_info))
+            self._logger.debug(message="File content:", code=file_content)
+            self._logger.section_end()
+        self._logger.section_end()
         return out
 
     def read_the_docs(self) -> list[tuple[DynamicFile, str]]:
-        info = self._path_manager.read_the_docs_config
+        self._logger.section("ReadTheDocs Configuration File")
+        file_info = self._path_manager.read_the_docs_config
         config = self._ccm["web"].get("readthedocs")
         if not config:
-            self._logger.skip("'readthedocs' not set in metadata.")
-            return [(info, "")]
-        text = pyserials.write.to_yaml_string(
-            data={
-                key: val for key, val in config.items()
-                if key not in ["name", "platform", "versioning_scheme", "language"]
-            },
-            end_of_file_newline=True
-        )
-        return [(info, text)]
+            file_content = ""
+        else:
+            file_content = pyserials.write.to_yaml_string(
+                data={
+                    key: val for key, val in config.items()
+                    if key not in ["name", "platform", "versioning_scheme", "language"]
+                },
+                end_of_file_newline=True
+            )
+        self._logger.info(message="File info:", code=str(file_info))
+        self._logger.debug(message="File content:", code=file_content)
+        self._logger.section_end()
+        return [(file_info, file_content)]
 
     def codecov_config(self) -> list[tuple[DynamicFile, str]]:
-        info = self._path_manager.codecov_config
+        self._logger.section("Codecov Configuration File")
+        file_info = self._path_manager.codecov_config
         config = self._ccm["workflow"].get("codecov")
         if not config:
-            self._logger.skip("'codecov' not set in metadata.")
-            return [(info, "")]
-        text = pyserials.write.to_yaml_string(data=config, end_of_file_newline=True)
-        try:
-            # Validate the config file
-            # https://docs.codecov.com/docs/codecov-yaml#validate-your-repository-yaml
-            pylinks.request(
-                verb="POST",
-                url="https://codecov.io/validate",
-                data=text.encode(),
-            )
-        except WebAPIError as e:
-            self._logger.error("Validation of Codecov configuration file failed.", str(e))
-        return [(info, text)]
+            file_content = ""
+        else:
+            file_content = pyserials.write.to_yaml_string(data=config, end_of_file_newline=True)
+            try:
+                # Validate the config file
+                # https://docs.codecov.com/docs/codecov-yaml#validate-your-repository-yaml
+                pylinks.request(
+                    verb="POST",
+                    url="https://codecov.io/validate",
+                    data=file_content.encode(),
+                )
+            except WebAPIError as e:
+                self._logger.error("Validation of Codecov configuration file failed.", str(e))
+        self._logger.info(message="File info:", code=str(file_info))
+        self._logger.debug(message="File content:", code=file_content)
+        self._logger.section_end()
+        return [(file_info, file_content)]
 
     def issue_template_chooser(self) -> list[tuple[DynamicFile, str]]:
-        info = self._path_manager.issue_template_chooser_config
+        self._logger.section("Issue Template Chooser Configuration File")
+        file_info = self._path_manager.issue_template_chooser_config
         file = {"blank_issues_enabled": self._ccm["issue"]["blank_enabled"]}
         if self._ccm["issue"].get("contact_links"):
             file["contact_links"] = self._ccm["issue"]["contact_links"]
-        text = pyserials.write.to_yaml_string(data=file, end_of_file_newline=True)
-        return [(info, text)]
+        file_content = pyserials.write.to_yaml_string(data=file, end_of_file_newline=True)
+        self._logger.info(message="File info:", code=str(file_info))
+        self._logger.debug(message="File content:", code=file_content)
+        self._logger.section_end()
+        return [(file_info, file_content)]
 
     def gitignore(self) -> list[tuple[DynamicFile, str]]:
-        info = self._path_manager.gitignore
+        self._logger.section("Gitignore File")
+        file_info = self._path_manager.gitignore
         local_dir = self._ccm["path"]["dir"]["local"]["root"]
-        text = "\n".join(
+        file_content = "\n".join(
             self._ccm["repo"].get("gitignore", [])
             + [
                 f"{local_dir}/**",
@@ -145,11 +171,15 @@ class ConfigFileGenerator:
                 f"!{local_dir}/**/README.md",
             ]
         )
-        return [(info, text)]
+        self._logger.info(message="File info:", code=str(file_info))
+        self._logger.debug(message="File content:", code=file_content)
+        self._logger.section_end()
+        return [(file_info, file_content)]
 
     def gitattributes(self) -> list[tuple[DynamicFile, str]]:
-        info = self._path_manager.gitattributes
-        text = ""
+        self._logger.section("Gitattributes File")
+        file_info = self._path_manager.gitattributes
+        file_content = ""
         attributes = self._ccm["repo"].get("gitattributes", [])
         max_len_pattern = max([len(list(attribute.keys())[0]) for attribute in attributes])
         max_len_attr = max(
@@ -159,5 +189,8 @@ class ConfigFileGenerator:
             pattern = list(attribute.keys())[0]
             attrs = list(attribute.values())[0]
             attrs_str = "  ".join(f"{attr: <{max_len_attr}}" for attr in attrs).strip()
-            text += f"{pattern: <{max_len_pattern}}    {attrs_str}\n"
-        return [(info, text)]
+            file_content += f"{pattern: <{max_len_pattern}}    {attrs_str}\n"
+        self._logger.info(message="File info:", code=str(file_info))
+        self._logger.debug(message="File content:", code=file_content)
+        self._logger.section_end()
+        return [(file_info, file_content)]
