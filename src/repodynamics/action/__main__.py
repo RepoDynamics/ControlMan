3f3098da8a6f6190cab92f99ede68d94e40050cf
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-30 06:48:51 +0100

    Update __main__.py

--- src/repodynamics/action/__main__.py
+++ src/repodynamics/action/__main__.py
@@ -1,4 +1,15 @@
+import actionman
+
 from repodynamics import action
 
 if __name__ == "__main__":
+    actionman.logger.create(
+        realtime_output=True,
+        github_console=True,
+        initial_section_number=2,
+        exit_code_critical=1,
+        output_html_filepath="log.html",
+        root_heading="Execute Action",
+        html_title="ProMan Action Log",
+    )
     action.run()
