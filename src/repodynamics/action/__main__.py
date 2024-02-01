84c39500f2dd362fc28fb9728e86022492ce715f
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-01 18:12:35 +0100

    Update __main__.py

--- src/repodynamics/action/__main__.py
+++ src/repodynamics/action/__main__.py
@@ -1,14 +1,19 @@
-import actionman
+import loggerman
 
 from repodynamics import action
+from repodynamics.exception import RepoDynamicsError
+
 
 if __name__ == "__main__":
-    actionman.logger.create(
-        realtime_output=True,
-        github_console=True,
-        initial_section_number=2,
+    loggerman.create(
+        global_=True,
+        realtime=True,
+        github=True,
+        init_section_number=2,
         exit_code_critical=1,
-        output_html_filepath="log.html",
+        sectioner_exception_catch=RepoDynamicsError,
+        sectioner_exception_log_level=loggerman.LogLevel.CRITICAL,
+        output_html_filepath="proman_action_log.html",
         root_heading="Execute Action",
         html_title="ProMan Action Log",
     )
