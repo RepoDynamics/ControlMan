20827729297ab523461351531f55f59240e326d3
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-03 10:42:00 +0100

    Update time.py

--- src/repodynamics/time.py
+++ src/repodynamics/time.py
@@ -1,13 +1,13 @@
-import datetime
+import datetime as _datetime
 
 
 _FORMAT = "%Y_%m_%d_%H_%M_%S"
 
 
 def now():
-    return datetime.datetime.now(tz=datetime.timezone.utc).strftime(_FORMAT)
+    return _datetime.datetime.now(tz=_datetime.timezone.utc).strftime(_FORMAT)
 
 
 def is_expired(timestamp: str, expiry_days: float) -> bool:
-    exp_date = datetime.datetime.strptime(timestamp, _FORMAT) + datetime.timedelta(days=expiry_days)
-    return exp_date <= datetime.datetime.now()
+    exp_date = _datetime.datetime.strptime(timestamp, _FORMAT) + _datetime.timedelta(days=expiry_days)
+    return exp_date <= _datetime.datetime.now()
