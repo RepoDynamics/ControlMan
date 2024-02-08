4ea61f6b5168471d43fe5c2133a396da34bd7eb8
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-08 22:58:28 +0100

    Update health.py

--- src/controlman/files/generator/health.py
+++ src/controlman/files/generator/health.py
@@ -1,6 +1,6 @@
 from actionman.logger import Logger
 
-from controlman._path import PathManager
+from controlman._path_manager import PathManager
 from controlman.datatype import DynamicFile
 from controlman.control.content import ControlCenterContentManager
 
