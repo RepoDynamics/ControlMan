a08303f61b0ca51894941e244bddf6bb7fd504a8
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-08 18:55:10 +0100

    Update exception.py

--- src/controlman/exception.py
+++ src/controlman/exception.py
@@ -1,5 +1,5 @@
-class RepoDynamicsError(Exception):
-    """Base class for exceptions in this module."""
+class ControlManException(Exception):
+    """Base class for all exceptions raised by ControlMan."""
 
     def __init__(self, message: str):
         self.message = message
@@ -7,9 +7,30 @@ class RepoDynamicsError(Exception):
         return
 
 
-class RepoDynamicsInternalError(RepoDynamicsError):
-    """An internal error occurred in RepoDynamics."""
+class ControlManContentException(ControlManException):
+    """Base class for all exceptions related to the user repository's control center contents."""
 
     def __init__(self, message: str):
+        super().__init__(message)
+        return
+
+
+class ControlManSchemaValidationError(ControlManContentException):
+    """Exception raised when a control center file is invalid against its schema."""
+
+    def __init__(
+        self, rel_path: str, file_ext: str, is_dir: bool, has_extension: bool
+    ):
+        if is_dir:
+            intro = f"One of the control center files in '{rel_path}'"
+            if has_extension:
+                intro += " (or in one of their extensions defined in 'extensions.yaml')"
+        else:
+            intro = f"The control center file at '{rel_path}.{file_ext}'"
+            if has_extension:
+                intro += " (or in one of its extensions defined in 'extensions.yaml')"
+        message = (
+            f"{intro} is invalid against its schema. Please check the error details below and fix the issue."
+        )
         super().__init__(message)
         return
