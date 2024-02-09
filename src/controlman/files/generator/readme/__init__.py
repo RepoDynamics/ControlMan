ae62989e37455bf8ff0ff17b028a66c1048d8984
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-02-09 12:44:40 +0100

    Update __init__.py

--- src/controlman/files/generator/readme/__init__.py
+++ src/controlman/files/generator/readme/__init__.py
@@ -1,10 +1,8 @@
-from actionman.logger import Logger
-
 from controlman.datatype import DynamicFile
-from controlman.control.content import ControlCenterContentManager
+from controlman import ControlCenterContentManager
 from controlman._path_manager import PathManager
 from controlman.files.generator.readme.main import ReadmeFileGenerator
-from controlman.files.generator import PyPackITDefaultReadmeFileGenerator
+from controlman.files.generator.readme.pypackit_default import PyPackITDefaultReadmeFileGenerator
 
 
 _THEME_GENERATOR = {
@@ -15,19 +13,18 @@ _THEME_GENERATOR = {
 def generate(
     content_manager: ControlCenterContentManager,
     path_manager: PathManager,
-    logger: Logger,
 ) -> list[tuple[DynamicFile, str]]:
     out = ReadmeFileGenerator(
-        content_manager=content_manager, path_manager=path_manager, target="repo", logger=logger
+        content_manager=content_manager, path_manager=path_manager, target="repo"
     ).generate()
     if content_manager["readme"]["repo"]:
         theme = content_manager["readme"]["repo"]["theme"]
         out.extend(_THEME_GENERATOR[theme](
-            content_manager=content_manager, path_manager=path_manager, target="repo", logger=logger
+            content_manager=content_manager, path_manager=path_manager, target="repo"
         ).generate())
     if content_manager["readme"]["package"]:
         theme = content_manager["readme"]["package"]["theme"]
         out.extend(_THEME_GENERATOR[theme](
-            content_manager=content_manager, path_manager=path_manager, target="package", logger=logger
+            content_manager=content_manager, path_manager=path_manager, target="package"
         ).generate())
     return out
