03522f856a4cb2149fd0ecb92f5152dd60e8a4d4
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-26 22:28:53 +0200

    Release version 0.0.0.dev297

--- src/controlman/exception/base.py
+++ src/controlman/exception/base.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING as _TYPE_CHECKING
-
+from functools import partial as _partial
 from exceptionman import ReporterException as _ReporterException
+import mdit as _mdit
 
 if _TYPE_CHECKING:
     from mdit import Document
@@ -12,13 +13,12 @@ class ControlManException(_ReporterException):
     """Base class for all exceptions raised by ControlMan."""
 
     def __init__(self, report: Document):
-        super().__init__(
-            report=report,
-            sphinx_config={
-                "extensions": ['myst_parser', 'sphinx_design', 'sphinx_togglebutton'],
-                "myst_enable_extensions": ["colon_fence", "fieldlist"],
-                "html_theme": "pydata_sphinx_theme",
-                "html_title": "ControlMan Error Report",
-            }
+        sphinx_config = {"html_title": "ControlMan Error Report"}
+        report.target_configs["sphinx"] = _mdit.target.sphinx(
+            renderer=_partial(
+                _mdit.render.sphinx,
+                config=_mdit.render.get_sphinx_config(sphinx_config)
+            )
         )
+        super().__init__(report=report)
         return
